e7ea78f5bdc386d53bcc147232684fb75c3c12cf
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-05-29 16:09:00 +0100

    update customer component

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {data} from "autoprefixer";
 import {CustomerService} from "../services/customer.service";
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import {Customer} from "../model/customer.model";
 import {catchError} from "rxjs";
 import {throwError} from "rxjs";
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -15,15 +16,47 @@ import {throwError} from "rxjs";
 export class CustomersComponent implements OnInit {
   customers: Observable<Array<Customer>> | undefined;
   errorMessage: string | undefined;
-  constructor(private customerService:CustomerService) { }
+  searchFormGroup ! : FormGroup;
+  constructor(private customerService:CustomerService,private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
-    this.customers = this.customerService.getCustomers().pipe(
+
+    this.searchFormGroup = this.formBuilder.group({
+      keyword : this.formBuilder.control("")
+    })
+    this.handleSearchCustomers();
+  }
+
+  handleSearchCustomers() {
+
+    let keyword = this.searchFormGroup.value.keyword;
+    this.customers = this.customerService.searchCustomer(keyword).pipe(
       catchError(err => {
         this.errorMessage = err.message;
         return throwError(err);
       })
     );
+  }
+
+  handleDeleteCustomer(customer:Customer) {
+    let conf = confirm("Are you sure")
+    if(!conf) return;
+    this.customerService.deleteCustomer(customer.id).subscribe({
+      next:data=>{
+        this.customers = this.customers?.pipe(
+          map(
+            data=>{
+              let index = data.indexOf(customer);
+              data.slice(index,1)
+              return data;
+            }
+          )
+        )
+      }
+      ,error:err=>{
+        console.log(err);
+      }
+    })
   }
 }
 
