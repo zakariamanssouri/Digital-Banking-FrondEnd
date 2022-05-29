58dc2c5b81957855e3c59a31b52aa0af3d8799db
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-05-29 16:10:21 +0100

    add customer service for searching ,deleting and saving new customers

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import { Observable } from 'rxjs';
 import {Customer} from "../model/customer.model";
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,6 +11,19 @@ export class CustomerService {
   constructor(private http:HttpClient) {
   }
   public getCustomers():Observable<Array<Customer>> {
-    return this.http.get<Array<Customer>>("http://localhost:9000/customers");
+    return this.http.get<Array<Customer>>(environment.URI+"/customers");
   }
+  
+  public searchCustomer(keyword:string):Observable<Array<Customer>> {
+    return this.http.get<Array<Customer>>(environment.URI+"/customers/search?keyword="+keyword);
+  }
+
+  public saveCustomer(customer:Customer):Observable<Customer> {
+    return this.http.post<Customer>(environment.URI+"/customers",customer);
+  }
+
+  public deleteCustomer(id:number) {
+    return this.http.delete(environment.URI+"/customers/"+id);
+  }
+  
 }
