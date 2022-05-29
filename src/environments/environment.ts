7e13067d049a535ffdae89537c72c391d664f473
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-05-29 16:10:41 +0100

    add backendURI

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  URI : "http://localhost:9000"
+  
 };
 
 /*
