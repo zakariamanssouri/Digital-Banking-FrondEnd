d6751dd6ec94f2b6edce0bac1ccddf853aa64720
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-05-31 00:21:41 +0100

    improve UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CustomersComponent} from "./customers/customers.component";
 import {AccountsComponent} from "./accounts/accounts.component";
+import { NewCustomerComponent } from './new-customer/new-customer.component';
 
 const routes: Routes = [
   {path:"customers", component:CustomersComponent},
+  {path:"new-customer", component:NewCustomerComponent},
   {path:"accounts", component:AccountsComponent},
 
   ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,19 @@ import { AccountsComponent } from './accounts/accounts.component';
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import { ReactiveFormsModule } from '@angular/forms';
 import { NewCustomerComponent } from './new-customer/new-customer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -22,7 +35,16 @@ import { NewCustomerComponent } from './new-customer/new-customer.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTableModule,
+    MatInputModule,
+    MatProgressSpinnerModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
